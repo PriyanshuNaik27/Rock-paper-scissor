e50a63c626824ce9d4b69d41dc4b6ea72b0774e2
PriyanshuNaik27 <151499141+PriyanshuNaik27@users.noreply.github.com>
2024-10-07 17:55:26 +0530

    Add files via upload

---
+++ rock paper scissor/app.js
@@ -0,0 +1,57 @@
+let userScore =0;
+let compScore =0;
+
+const msg = document.querySelector("#click");
+const choices = document.querySelectorAll(".choice");
+
+const genComputerChoice = () =>{
+    const options = ["rock","paper","scissor"];
+    return options[Math.floor(Math.random()*3)];
+}
+
+const  winner = (userwin) =>{
+    if(userwin){
+        console.log("you win ...");
+        msg.innerText="YOU WIN";
+        
+    }
+    else {
+        console.log("you lose...");
+        msg.innerText="YOU LOSE";
+    }
+}
+const draw = () =>{
+    console.log("game was draw");
+    msg.innerText="DRAW HOGYA BHAI";
+}
+
+const playGame = (userChoice) => {
+    console.log("user choice =", userChoice)
+    const compChoice = genComputerChoice();
+    console.log("computer choice was",compChoice);
+    if(userChoice===compChoice){
+        draw();
+    }
+    else{
+        let userwin =true;
+        if(userChoice=== "rock"){
+            userwin = compChoice ==="paper" ? false:true;
+        }
+         else if(userChoice=== "rock"){
+            userwin = compChoice ==="scissor" ? false:true;
+        }
+        else {
+            userwin= compChoice ==="rock"? false : true
+        }
+         winner(userwin);
+    }
+};
+choices.forEach((choice) =>{
+    choice.addEventListener("click",()=>{
+        console.log("choice was clicked");
+      
+        const userChoice =choice.getAttribute("id")
+        playGame(userChoice)
+    });
+});
+
